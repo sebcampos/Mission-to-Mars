6fa5edc8dbcdf2583c6274cb75c358101df7fb4e
sebcampos <sebcampos@criptext.com>
2020-11-17 15:22:42 -0800

    Starting on deliverable 2

--- scraping.py
+++ scraping.py
@@ -23,6 +23,7 @@ def scrape_all():
         "news_paragraph": news_paragraph,
         "featured_image": featured_image(browser),
         "facts": mars_facts(),
+        "Png urls & titles":scrape_urls_and_titles(browser), 
         "last_modified": dt.datetime.now()
     }
 
@@ -105,6 +106,37 @@ def mars_facts():
 
     # Convert dataframe into HTML format, add bootstrap
     return df.to_html(classes="table table-striped")
+
+def scrape_urls_and_titles(browser):
+    # 1. Use browser to visit the URL 
+    url = 'https://astrogeology.usgs.gov/search/results?q=hemisphere+enhanced&k1=target&v1=Mars'
+    browser.visit(url)
+
+    base_url = 'https://astrogeology.usgs.gov/'
+
+    # 2. Create a list to hold the images and titles.
+    hemisphere_image_urls = []
+
+    # 3. Write code to retrieve the image urls and titles for each hemisphere.
+
+    #create soup object
+    webpage_soup = soup(browser.html,"html.parser")
+
+    #create list object conataining all relative 'divs'
+    divs_list = webpage_soup.find_all('div', class_='item')
+
+    #iterate through divs_list object to collect relevent info ie titles, descriptions, and img links
+    titles = [i.find('h3').get_text() for i in divs_list]
+    descriptions = [i.find('p').get_text() for i in divs_list]
+
+    # use a for loops to navigate to each item in the divs_list and scrape the img url by title name using browser.find_by_text()
+    for i in titles:
+        browser.find_by_text(i).click()
+        hot_cauldren = soup(browser.html,"html.parser")
+        hemisphere_image_urls.append({'img_url':f"{base_url}{hot_cauldren.find('img', class_='wide-image').get('src')}",'title':i})
+        browser.visit(url)
+    
+    return hemisphere_image_urls
 
 if __name__ == "__main__":
 
